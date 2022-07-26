f084edafc1c3293f78feaff3443a8105513fd6bd
Alexey Kerpel <alexeykerpel15@gmail.com>
2022-07-26 17:34:33 +0300

    Fixed file structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["ideas"]);
+
+  const renderItems = () => {
+    return data?.items.map(({ id, title, description }) => (
+      <div key={id}>
+        <div className="text-2xl font-bold underline">{title}</div>
+        <div className="text-1xl">{description}</div>
+      </div>
+    ));
+  };
+
+  return <div>{!isLoading ? renderItems() : "Loading.."}</div>;
+};
+
+export default Home;
